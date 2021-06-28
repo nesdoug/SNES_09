35930d548024ad7b807f6a05bee1cc5aec636750
nesdoug <dougfraker@gmail.com>
2021-06-28 16:40:14 -0400

    update 2.0
    update 2.0

--- main.asm
+++ main.asm
@@ -5,79 +5,8 @@
 
 
 
-
-
-
-.segment "ZEROPAGE"
-
-temp1: .res 2
-temp2: .res 2
-temp3: .res 2
-temp4: .res 2
-temp5: .res 2
-temp6: .res 2
-
-; for sprite code
-sprid: .res 1
-spr_x: .res 2 ; 9 bit
-spr_y: .res 1 
-spr_c: .res 1 ; tile #
-spr_a: .res 1 ; attributes
-spr_sz:	.res 1 ; sprite size, 0 or 2
-spr_h: .res 1 ; high 2 bits
-spr_x2:	.res 2 ; for meta sprite code
-
-; for sprite collision code
-obj1x: .res 1
-obj1w: .res 1
-obj1y: .res 1
-obj1h: .res 1
-obj2x: .res 1
-obj2w: .res 1
-obj2y: .res 1
-obj2h: .res 1
-collision: .res 1
-
-pad1: .res 2
-pad1_new: .res 2
-pad2: .res 2
-pad2_new: .res 2
-in_nmi: .res 2
-
-
-cube_x: .res 1
-cube_x_sp: .res 1 ;speed
-cube_y: .res 1
-cube_y_sp: .res 1
-
-cube_x_old: .res 1
-cube_y_old: .res 1
-
-cube_dir: .res 1 ;face left 0 or right 1
-
-;for BG collision code
-obj_top: .res 1
-obj_bottom: .res 1
-obj_left: .res 1
-obj_right: .res 1
-
-bright_var:	.res 1
-
-
-FACE_LEFT = 0
-FACE_RIGHT = 1
-
-
-.segment "BSS"
-
-PAL_BUFFER: .res 512
-
-OAM_BUFFER: .res 512 ;low table
-OAM_BUFFER2: .res 32 ;high table
-
-
-
-.include "defines.asm"
+.include "regs.asm"
+.include "variables.asm"
 .include "macros.asm"
 .include "init.asm"
 .include "library.asm"
@@ -90,39 +19,25 @@ OAM_BUFFER2: .res 32 ;high table
 .segment "CODE"
 
 ; enters here in forced blank
-main:
-.a16 ; just a standardized setting from init code
+Main:
+.a16 ; the setting from init code
 .i16
 	phk
 	plb
-	jsr oam_clear
 	
 	
 ; COPY PALETTES to PAL_BUFFER	
 ;	BLOCK_MOVE  length, src_addr, dst_addr
 	BLOCK_MOVE  512, BG_Palette, PAL_BUFFER
-	
+	A8 ;block move will put AXY16. Undo that.
 	
 ; DMA from PAL_BUFFER to CGRAM
-	A8
-	stz pal_addr ; $2121 cg address = zero
-
-	stz $4300 ; transfer mode 0 = 1 register write once
-	lda #$22  ; $2122
-	sta $4301 ; destination, pal data
-	ldx #.loword(PAL_BUFFER)
-	stx $4302 ; source
-	lda #^PAL_BUFFER
-	sta $4304 ; bank
-	ldx #512 ; full palette size
-	stx $4305 ; length
-	lda #1
-	sta $420b ; start dma, channel 0
+	jsr DMA_Palette ; in init.asm
 	
 	
 ; DMA from Tiles to VRAM	
 	lda #V_INC_1 ; the value $80
-	sta vram_inc  ; $2115 = set the increment mode +1
+	sta VMAIN  ; $2115 = set the increment mode +1
 
 	DMA_VRAM  (End_BG_Tiles - BG_Tiles), BG_Tiles, $0000
 	
@@ -144,52 +59,54 @@ main:
 	
 
 	lda #1 ; mode 1, tilesize 8x8 all
-	sta bg_size_mode ; $2105
+	sta BGMODE ; $2105
 	
-	stz bg12_tiles ; $210b BG 1 and 2 TILES at VRAM address $0000
+	stz BG12NBA ; $210b BG 1 and 2 TILES at VRAM address $0000
 	
 	lda #$60 ; bg1 map at VRAM address $6000
-	sta tilemap1 ; $2107
+	sta BG1SC ; $2107
 	
 	lda #$68 ; bg2 map at VRAM address $6800
-	sta tilemap2 ; $2108
+	sta BG2SC ; $2108
 	
 	lda #$70 ; bg3 map at VRAM address $7000
-	sta tilemap3 ; $2109	
+	sta BG3SC ; $2109	
 	
 	lda #2 ;sprite tiles at $4000
-	sta spr_addr_size ;= $2101
+	sta OBSEL ;= $2101
 
 ;allow everything on the main screen	
 	lda #BG1_ON|SPR_ON ; $11
-	sta main_screen ; $212c
+	sta TM ; $212c
 	
 	;turn on NMI interrupts and auto-controller reads
 	lda #NMI_ON|AUTO_JOY_ON
-	sta $4200
+	sta NMITIMEN ;$4200
 	
 	lda #FULL_BRIGHT ; $0f = turn the screen on, full brighness
-	sta fb_bright ; $2100
+	sta INIDISP ; $2100
 	sta bright_var
 
 
-
+;set some initial values
+	lda #$30
+	sta cube_x
+	sta cube_y
 	
 	
 	
-	
-InfiniteLoop:	
+Infinite_Loop:	
 ;game loop a8 xy16
 	A8
 	XY16
-	jsr wait_nmi ;wait for the beginning of v-blank
-	jsr dma_oam  ;copy the OAM_BUFFER to the OAM
+	jsr Wait_NMI ;wait for the beginning of v-blank
+	jsr DMA_OAM  ;copy the OAM_BUFFER to the OAM
 	
 	lda bright_var
-	sta fb_bright ; $2100
+	sta INIDISP ; $2100
 	
-	jsr pad_poll ;read controllers
-	jsr oam_clear
+	jsr Pad_Poll ;read controllers
+	jsr Clear_OAM
 
 	
 	
@@ -276,7 +193,7 @@ X_Move:
 
 ;make a hitbox a little smaller than the object	
 	A8
-	jsr load_hitbox
+	jsr Load_Hitbox
 	
 ;check collision, then revert if yes
 	AXY8
@@ -286,12 +203,12 @@ X_Move:
 ;check 2 points on the left
 	ldx obj_left
 	ldy obj_top
-	jsr hit_bg
+	jsr Hit_BG
 	cmp #1
 	beq @handle_collision
 	ldx obj_left
 	ldy obj_bottom
-	jsr hit_bg
+	jsr Hit_BG
 	cmp #1
 	beq @handle_collision
 	bra Y_Move
@@ -300,12 +217,12 @@ X_Move:
 ;check 2 points on the right
 	ldx obj_right
 	ldy obj_top
-	jsr hit_bg
+	jsr Hit_BG
 	cmp #1
 	beq @handle_collision
 	ldx obj_right
 	ldy obj_bottom
-	jsr hit_bg
+	jsr Hit_BG
 	cmp #1
 	bne Y_Move
 	
@@ -337,7 +254,7 @@ Y_Move:
 
 ;make a hitbox a little smaller than the object	
 	A8
-	jsr load_hitbox
+	jsr Load_Hitbox
 	
 ;check collision, then revert if yes
 	AXY8
@@ -347,12 +264,12 @@ Y_Move:
 ;check 2 points on the top
 	ldx obj_left
 	ldy obj_top
-	jsr hit_bg
+	jsr Hit_BG
 	cmp #1
 	beq @handle_collision
 	ldx obj_right
 	ldy obj_top
-	jsr hit_bg
+	jsr Hit_BG
 	cmp #1
 	beq @handle_collision
 	bra Past_Move
@@ -361,12 +278,12 @@ Y_Move:
 ;check 2 points on the bottom
 	ldx obj_left
 	ldy obj_bottom
-	jsr hit_bg
+	jsr Hit_BG
 	cmp #1
 	beq @handle_collision
 	ldx obj_right
 	ldy obj_bottom
-	jsr hit_bg
+	jsr Hit_BG
 	cmp #1
 	bne Past_Move
 	
@@ -391,7 +308,7 @@ Past_Move:
 	clc
 	adc #7
 	tay
-	jsr hit_bg
+	jsr Hit_BG
 	cmp #2
 	bne Past_Yellow
 	lda #$07 ;darker
@@ -400,12 +317,12 @@ Past_Move:
 Past_Yellow:
 	XY16
 
-	jsr draw_sprites
-	jmp InfiniteLoop
+	jsr Draw_Sprites
+	jmp Infinite_Loop
 	
 	
 	
-load_hitbox:
+Load_Hitbox:
 ;makes a hitbox a little smaller than the sprite.
 .a8
 	lda cube_y
@@ -426,7 +343,7 @@ load_hitbox:
 	
 	
 	
-hit_bg:
+Hit_BG:
 ;in values 
 ;x = x position in pixels
 ;y = y position in pixels
@@ -450,11 +367,13 @@ hit_bg:
 	
 
 	
-draw_sprites:
+Draw_Sprites:
 	php
 	
 	A8
 	XY16
+	stz sprid
+	
 	lda cube_x
 	sta spr_x
 	stz spr_x+1
@@ -476,40 +395,17 @@ draw_sprites:
 @both:
 .a16
 .i16
-	jsr oam_meta_spr
+	jsr OAM_Meta_Spr
 
 	plp
 	rts
 	
-	
-	
-clear_sp_buffer:
-.a8
-.i16
-	php
-	A8
-	XY16
-	lda #224 ;put all y values just below the screen
-	ldx #$0000
-	ldy #128 ;number of sprites
-@loop:
-	sta OAM_BUFFER+1, x
-	inx
-	inx
-	inx
-	inx ;add 4 to x
-	dey
-	bne @loop
-	plp
-	rts
-	
-	
-	
+
 
 	
 	
 	
-wait_nmi:
+Wait_NMI:
 .a8
 .i16
 ;should work fine regardless of size of A
@@ -523,31 +419,10 @@ wait_nmi:
 	rts
 
 	
-dma_oam:
-.a8
-.i16
-	php
-	A8
-	XY16
-	ldx #$0000
-	stx oam_addr_L ;$2102 (and 2103)
-	
-	stz $4300 ; transfer mode 0 = 1 register write once
-	lda #4 ;$2104 oam data
-	sta $4301 ; destination, oam data
-	ldx #.loword(OAM_BUFFER)
-	stx $4302 ; source
-	lda #^OAM_BUFFER
-	sta $4304 ; bank
-	ldx #544
-	stx $4305 ; length
-	lda #1
-	sta $420b ; start dma, channel 0
-	plp
-	rts
+
 	
 	
-pad_poll:
+Pad_Poll:
 .a8
 .i16
 ; reads both controllers to pad1, pad1_new, pad2, pad2_new
