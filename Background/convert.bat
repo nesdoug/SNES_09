dfbb384da08ec1a131ac4c428d3b1bb3b5894860
nesdoug <dougfraker@gmail.com>
2021-10-30 13:16:43 -0400

    edit
    edit

--- Background/convert.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set name="green"
-set name2="red"
-
-superfamiconv -B 4 -i %name%.png -p %name%.pal -t %name%.chr
-superfamiconv -B 4 -i %name2%.png -p %name2%.pal -t %name2%.chr
-
-pause
--- Sprites/convert.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set name="Cube"
-set name2="red"
-
-superfamiconv -B 4 -i %name%.png -p %name%.pal -t %name%.chr
-
-pause
